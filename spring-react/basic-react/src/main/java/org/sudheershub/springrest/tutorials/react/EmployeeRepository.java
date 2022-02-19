c83c7571e7d82c4d6308134de75cc95045ec2ef7
sudheershub <98266310+sudheershub@users.noreply.github.com>
2022-02-19 19:33:12 +0000

    delete duplicate
    delete basic react as it is duplicate

--- spring-react/basic-react/src/main/java/org/sudheershub/springrest/tutorials/react/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.sudheershub.springrest.tutorials.react;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
